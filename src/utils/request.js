5ad540b24eb2f9dab849803c6afc8ef69e666e39
Lazy <lz.lazy.cn@gmail.com>
2025-12-17 05:59:46 +0800

    修改 API地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API || 'https://dev.xiangtaihou-food.com/traceability-code-service/admin', // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API || 'https://h5.xiangtaihou-food.com/traceability-code-service/admin', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
